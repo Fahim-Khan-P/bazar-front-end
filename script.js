8d0900bdcad6f27c6681c6f655df16f41c8c7ed9
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-08-27 13:18:14 +0430

    Save location in session storage and update the href from session storage!

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const provinceSelect = document.querySelector('select[name="province"]');
-  const defaultHrefs = [];
-  const categories = document.querySelectorAll("a.category");
-  for (let i = 0; i < categories.length; i++) {
-      defaultHrefs[i] = categories[i].getAttribute("href");
-  }
-  provinceSelect.addEventListener("change", function (e) {
+document.addEventListener("DOMContentLoaded", () => {
+    const provinceSelect = document.querySelector('select[name="province"]');
+    const categories = document.querySelectorAll("a.category");
+  
+    // get province from session storage using get Itrm method 
+    const selectedProvince = sessionStorage.getItem("selectedProvince");
+  
+    // check the seesion storage for saved province 
+    if (selectedProvince) {
+      provinceSelect.value = selectedProvince;
+    //   update the href of category from sessionStorage 
+      updateCategoryURLs(selectedProvince);
+    }
+  
+    provinceSelect.addEventListener("change", (e) => {
       e.preventDefault();
       const selectedProvince = provinceSelect.value;
-      updateHref(selectedProvince);
-      console.log(selectedProvince)
+  
+    //update and save location in sessionStorage while dropdown changing  
+      sessionStorage.setItem("selectedProvince", selectedProvince);
+    });
+  
+    function updateCategoryURLs(selectedProvince) {
+      categories.forEach((category) => {
+        const href = `${category.getAttribute("href")}/${selectedProvince}`;
+        category.setAttribute("href", href);
+      });
+    }
   });
-  function updateHref(selectedProvince) {
-      for (let i = 0; i < categories.length; i++) {
-          categories[i].setAttribute("href", defaultHrefs[i]);
-          const href =
-              categories[i].getAttribute("href") + "/" + selectedProvince;
-          categories[i].setAttribute("href", href);
-          console.log(href);
- }
-}
-});
-
